e39da2292c3053cdcbbf4b8bc1293f3bdd77e365
Jan D <79925808+dark-r00t@users.noreply.github.com>
2023-04-04 00:25:55 -0400

    Update builtin.h

--- include/builtin.h
+++ include/builtin.h
@@ -6,7 +6,7 @@
 typedef __builtin_va_list va_list;
 
 #define my_va_start(v, l)   __builtin_va_start(v,l)
-#define my_va_end(v)       __builtin_va_end(v)
+#define my_va_end(v)        __builtin_va_end(v)
 #define my_va_arg(v, l)     __builtin_va_arg(v,l)
 
 #endif //SBRK_BUILTIN_H
